348d3d3b1c10561930dcfb5817d05f0a4367887d
ayana1709 <ayanabashachali@gmail.com>
2024-05-11 20:07:45 +0300

    dashboard editing

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller{
 
     public function dashboard(Request $request){
-        echo"login";  
+        return view('backend.dashboard.list');
 
 
     }
