d78b4fdfff9774c48d4d1029f40df3a0ae0adc6e
ayana1709 <ayanabashachali@gmail.com>
2024-05-20 22:25:54 +0300

    finilising create table for job

--- resources/views/backend/jobs/list.blade.php
+++ resources/views/backend/jobs/list.blade.php
@@ -34,20 +34,26 @@
                          <form method="get" action="">
                           <div class="card-body">
                             <div class="row">
-                            <div class="form-group col-md-1">
+                            <div class="form-group col-md-3">
                                 <label>
                                 Job Title
                                 </label>
-                                <input type="number" name="id" class="form-control" value="{{Request()->job_title}}" placeholder="ID">
+                                <input type="number" name="job_title" class="form-control" value="{{Request()->job_title}}" placeholder="ID">
+</div>
+<div class="form-group col-md-3">
+                                <label>
+                              Min Salary
+                                </label>
+                                <input type="number" name="min_salary" class="form-control" value="{{Request()->min_salary}}" placeholder="min salary">
+</div>
+<div class="form-group col-md-3">
+                                <label>
+                              Max Salary
+                                </label>
+                                <input type="number" name="max_salary" class="form-control" value="{{Request()->max_salary}}" placeholder="max salary">
 </div>
 
                              
-                              <div class="form-group col-md-3">
-                                <label>
-                                  Email ID
-                                </label>
-                                <input type="text" name="email" value="{{Request()->email}}" class="form-control" placeholder="Emai ID">
-                              </div>
 
                               <div class="form-group col-md-3">
                                 <button class="btn btn-primary" type="submit" style="margin-top:30px;">search</button> 
@@ -77,56 +83,48 @@
           <tr>
             <th>ID</th>
             <th>Job Title</th> 
-            
+            <th>Min Salary</th> 
+            <th>Max Salary</th> 
+            <th>Created At</th>
             <th>Action</th>
 
-
-
-            
-
-            
           </tr>
         </thead>
         <tbody>
      
-                 <tr>
-                 <td>1</td>
+            <tr>
+                  <td>1</td>
                   <td>title</td>
-                
-                
-                <td>
+                  <td>200</td>
+                  <td>300</td>
+                  <td>100</td>
+                  <td>10-12-2023 </td>
+                  <td>
                   <a href="" class="btn btn-info">View</a>
                   <a href="" class="btn btn-primary">Edit</a>
                   <a href="" 
                   onclick="return confirm('Are you sure u want to delete?')" class="btn btn-danger">Delete</a> 
-                </td>
-
-
-                  </tr>
-       
-         <tr>
-         <td colspan="100%"> No Record Found</td>
-         </tr>
+                   </td>
+              </tr>
+              <tr>
+                 <td colspan="100%"> No Record Found</td>
+              </tr>
          
         </tbody>
       </table>
       <div style="padding:10px; float:right;">
-    
-
       </div>
     </div>
    </div>
-
-                </section>
-        </div>
+</section>
+</div>
 
 
         <!-- content -->
-    </div>
+</div>
 <!-- content-wrapper -->
-
-
 </section>
+@endsection
 
 
 
