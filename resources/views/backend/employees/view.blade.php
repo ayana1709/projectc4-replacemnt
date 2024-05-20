8a426e7ec7784794cf629b0ec63267275178fe16
ayana1709 <ayanabashachali@gmail.com>
2024-05-20 09:45:47 +0300

    Employees pass data Route to view

--- resources/views/backend/employees/view.blade.php
+++ resources/views/backend/employees/view.blade.php
@@ -42,13 +42,119 @@
                                         
                                     </div>
                                 </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  First Name<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->name}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Last Name<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->last_name}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Email ID<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->email}}
+                                        
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Phone Number<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->Phone_number}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Hire Date<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->hire_date}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Job ID<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->job_id}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Salary<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->salary}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Commission Pct<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->comssion_pct}}
+                                        
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Manager ID<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->manager_id}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Department Id<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->departmnet_id}}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  IS Role<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{!empty($getRecord->is_role)? 'HR': 'Employees'}}
+                                         
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+  Created At<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->created_at }}
+                                        
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+ <label  class="col-sm-2 col-form-lable">
+ Updated_At<span style="color: red;"></span>
+                                    </label>
+                                    <div class="col-sm-10">{{$getRecord->updated_at}}
+                                        
+                                    </div>
+                                </div>
 
 
-                                <div class="card-footer">
+                                 <div class="card-footer">
   <a href="{{url('admin/employees')}}" class="btn btn-default ">Back</a>
      
-        Add
-    </button>
+        
+    
     
   </div>
 
